f8bedc05cb66a692a2f8968e1e6c574575387a44
RavindraBhoi <bhoiravindra11@gmail.com>
2024-09-23 18:07:06 +0530

    Update archinstall.sh

--- archinstall.sh
+++ archinstall.sh
@@ -1,62 +1,75 @@
 #!/usr/bin/env bash
 
-echo "Please enter root password:"
-read ROOTPASSWD
+echo "Please enter EFI paritition: (example /dev/sda1 or /dev/nvme0n1p1)"
+read EFI
 
-echo "Please Username:"
-read USER
+echo "Please enter Root(/) paritition: (example /dev/sda3)"
+read ROOT  
 
-echo "Please enter User password"
-read PASSWD
+echo "Please enter your Username"
+read USER 
 
-echo -e "\nCreating Filesystem...\n"
+echo "Please enter your Full Name"
+read NAME 
 
-mkfs.vfat -F32 -n /dev/sda1
-mkfs.ext4 -L /dev/sda2
+echo "Please enter your Password"
+read PASSWORD 
 
-mount /dev/sda2 /mnt
+# make filesystems
+echo -e "\nCreating Filesystems...\n"
+
+mkfs.fat -F32 "${EFI}"
+mkfs.ext4 "${ROOT}"
+
+# mount target
+mount "${ROOT}" /mnt
 mkdir /mnt/boot
-mount /dev/sda1 /mnt/boot
+mount "${EFI}" /mnt/boot/efi
 
-echo "__________________________________________________"
-echo "INSTALLING ARCH BASE ON MAIN DRIVE"
-echo "___________________________________________________"
+echo "--------------------------------------"
+echo "-- INSTALLING Base Arch Linux --"
+echo "--------------------------------------"
+pacstrap /mnt base base-devel linux linux-firmware linux-headers networkmanager sudo nano intel-ucode bluez bluez-utils git --noconfirm --needed
 
-pacstrap -i /mnt base base-devel linux linux-headers linux-firmware intel-ucode sudo git nano vim neofetch htop bluez bluez-utils networkmanager cargo --noconfirm --needed
-
+# fstab
 genfstab -U /mnt >> /mnt/etc/fstab
 
-cat <<READLEND> /mnt/next.sh
-passwd
-"$(ROOTPASSWD)"
-"$(ROOTPASSWD)"
-useradd -m -g users -G wheel,storage,video,audio -s /bin/bash "$(USER)"
-passwd "$(USER)"
-"$(PASSWD)"
-"$(PASSWD)"
+cat <<REALEND > /mnt/next.sh
+useradd -m $USER
+usermod -c "${NAME}" $USER
+usermod -aG wheel,storage,power,audio,video $USER
+echo $USER:$PASSWORD | chpasswd
 sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
-ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
-hwclock --systohc
-echo "Generating Locales......"
+
+echo "-------------------------------------------------"
+echo "Setup Language to US and set locale"
+echo "-------------------------------------------------"
 sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
-echo "archlinux" >> /etc/hostname
 
-cat <<EOF> /etc/hosts
-127.0.0.1  localhost
-::1        localhost
-127.0.1.1  archlinux.localdomain  archlinux
+ln -sf /usr/share/zoneinfo/Asia/Kathmandu /etc/localtime
+hwclock --systohc
+
+echo "archlinux" > /etc/hostname
+cat <<EOF > /etc/hosts
+127.0.0.1	localhost
+::1			localhost
+127.0.1.1	archlinux.localdomain	archlinux
 EOF
 
+echo "--------------------------------------"
+echo "-- Bootloader Installation  --"
+echo "--------------------------------------"
+
 pacman -S grub efibootmgr dosfstools mtools --noconfirm --needed
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id="Arch Linux"
 grub-mkconfig -o /boot/grub/grub.cfg
-systemctl enable bluetooth
-systemctl enable NetworkManager
+
+systemctl enable NetworkManager bluetooth
+echo "you can shutdown now"
 exit
-READLEND
+REALEND
 
 arch-chroot /mnt sh next.sh
 umount -lR /mnt
-echo "NOW SHUTDOWN YOUR SYSTEM USING COMMAND shutdown now"
