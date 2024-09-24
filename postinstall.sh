1a0d87b8f53c2251bf66060c30642d1772059da9
RavindraBhoi <bhoiravindra11@gmail.com>
2024-09-24 16:47:29 +0545

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -90,7 +90,7 @@ else [[ $BOOT == 11 ]];
   sudo pacman -Sy mate xorg sddm kitty
 fi
 
-sudo systemctl enable sddm kitty
+sudo systemctl enable sddm
 sudo pacman -S os-prober --noconfirm --needed
 sudo sed -i 's/^#GRUB_DISABLE_OS_PROBER=false/GRUB_DISABLE_OS_PROBER=false/' /etc/default/grub
 sudo grub-mkconfig -o /boot/grub/grub.cfg
