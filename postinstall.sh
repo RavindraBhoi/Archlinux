f486a7f5d08efe5c0c7046e7bee2eeb1802f3ceb
RavindraBhoi <bhoiravindra11@gmail.com>
2024-09-24 11:21:24 +0545

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -16,77 +16,77 @@ while true; do
     if [[ $BOOT == 1 || $BOOT == 2 || $BOOT == 3 || $BOOT == 4 || $BOOT == 5 || $BOOT == 6 || $BOOT == 7 || $BOOT == 8 || $BOOT == 9 || $BOOT == 10 || $BOOT == 11 ]]; then
         break
     else
-        echo "_____________________"
+        echo "---------------------"
         echo " GOLI BETA MASTI NAHI"
-        echo "_____________________"
+        echo "---------------------"
         echo "Invalid input. enter from 1 to 11"
     fi
 done
 
 if [[ $BOOT == 1 ]];
 then
-  echo "________________"
+  echo "----------------"
   echo "you chose Plasma"
-  echo "________________"
+  echo "----------------"
   sudo pacman -Sy plasma-meta plasma-workspace xorg sddm
 elif [[ $BOOT == 2 ]];
 then
-  echo "________________"
+  echo "----------------"
   echo "you chose GNOME"
-  echo "________________"
+  echo "----------------"
   sudo pacman -Sy gnome xorg sddm
 elif [[ $BOOT == 3 ]];
 then
-  echo "________________"
+  echo "----------------"
   echo "you chose XFCE4"
-  echo "________________"
+  echo "----------------"
   sudo pacman -Sy xfce4 xorg sddm
 elif [[ $BOOT == 4 ]];
 then
-  echo "________________"
+  echo "----------------"
   echo "you chose Cinnamon"
-  echo "________________"
+  echo "----------------"
   sudo pacman -Sy cinnamon xorg sddm
 elif [[ $BOOT == 5 ]];
 then
-  echo "________________"
+  echo "----------------"
   echo "you chose Cosmic"
-  echo "________________"
+  echo "----------------"
   sudo pacman -Sy cosmic xorg sddm
 elif [[ $BOOT == 6 ]];
 then
-  echo "________________"
+  echo "----------------"
   echo "you chose Deepin"
-  echo "________________"
+  echo "----------------"
   sudo pacman -Sy deepin xorg sddm
 elif [[ $BOOT == 7 ]];
 then
-  echo "________________"
+  echo "----------------"
   echo "you chose Budgie"
-  echo "________________"
+  echo "----------------"
   sudo pacman -Sy budgie xorg sddm
 elif [[ $BOOT == 8 ]];
 then
-  echo "________________"
+  echo "----------------"
   echo "you chose Cutefish"
-  echo "________________"
+  echo "----------------"
   sudo pacman -Sy cutefish xorg sddm
 elif [[ $BOOT == 9 ]];
 then
-  echo "________________"
+  echo "----------------"
   echo "you chose Enlightenment"
-  echo "________________"
+  echo "----------------"
   sudo pacman -Sy enlightenment xorg sddm
 elif [[ $BOOT == 10 ]];
 then
-  echo "________________"
+  echo "----------------"
   echo "you chose GNOME-Flashback"
-  echo "________________"
+  echo "----------------"
   sudo pacman -Sy gnome-flashback xorg sddm
 else [[ $BOOT == 11 ]];
-  echo "________________"
+  echo "----------------"
   echo "you chose Mate"
-  echo "________________"
+  echo "----------------"
   sudo pacman -Sy mate xorg sddm
 fi
 
