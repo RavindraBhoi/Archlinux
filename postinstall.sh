50f63d0cdd20ce1b90a2db125ed62781b8b119bf
RavindraBhoi <bhoiravindra11@gmail.com>
2024-09-24 17:23:27 +0545

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -87,10 +87,10 @@ else [[ $BOOT == 11 ]];
   echo "----------------"
   echo "you chose Mate"
   echo "----------------"
-  sudo pacman -Sy mate xorg sddm kitty
+  sudo pacman -Sy mate xorg lemurs kitty
 fi
 
-sudo systemctl enable sddm
+sudo systemctl enable lemurs
 sudo pacman -S os-prober --noconfirm --needed
 sudo sed -i 's/^#GRUB_DISABLE_OS_PROBER=false/GRUB_DISABLE_OS_PROBER=false/' /etc/default/grub
 sudo grub-mkconfig -o /boot/grub/grub.cfg
