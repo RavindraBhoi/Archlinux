fe3cf77f4b1673f5a64ba2ad849e57c559c9fdad
RavindraBhoi <bhoiravindra11@gmail.com>
2024-09-27 18:50:57 +0545

    Update archinstall.sh

--- archinstall.sh
+++ archinstall.sh
@@ -48,7 +48,7 @@ sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 
-ln -sf /usr/share/zoneinfo/Asia/Kathmandu /etc/localtime
+ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
 hwclock --systohc
 
 echo "archlinux" > /etc/hostname
