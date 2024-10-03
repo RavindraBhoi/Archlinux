2ce67c313de52e39982fe5b357b5f940cafab627
RavindraBhoi <bhoiravindra11@gmail.com>
2024-10-03 13:43:28 +0530

    Update and rename postinstall1.sh to Alacritty_neon.sh

---
+++ Alacritty_neon.sh
@@ -0,0 +1,39 @@
+cd .config
+mkdir alacritty
+cd alacritty
+touch alacritty.toml
+echo "colors.draw_bold_text_with_bright_colors = true" > alacritty.toml
+echo "[colors.primary]" >> alacritty.toml
+echo "background = '0x000b1e'" >> alacritty.toml
+echo "foreground = '0x0abdc6'" >> alacritty.toml
+echo "[colors.normal]" >> alacritty.toml
+echo "black = '0x123e7c'" >> alacritty.toml
+echo "red = '0xff0000'" >> alacritty.toml
+echo "green = '0xd300c4'" >> alacritty.toml
+echo "yellow = '0xf57800'" >> alacritty.toml
+echo "blue = '0x123e7c'" >> alacritty.toml
+echo "magenta = '0x711c91'" >> alacritty.toml
+echo "cyan = '0x0abdc6'" >> alacritty.toml
+echo "white = '0xd7d7d5'" >> alacritty.toml
+echo "[colors.bright]" >> alacritty.toml
+echo "black = '0x1c61c2'" >> alacritty.toml
+echo "red = '0xff0000'" >> alacritty.toml
+echo "green = '0xd300c4'" >> alacritty.toml
+echo "yellow = '0xf57800'" >> alacritty.toml
+echo "blue = '0x00ff00'" >> alacritty.toml
+echo "magenta = '0x711c91'" >> alacritty.toml
+echo "cyan = '0x0abdc6'" >> alacritty.toml
+echo "white = '0xd7d7d5'" >> alacritty.toml
+echo "[colors.dim]" >> alacritty.toml
+echo "black = '0x1c61c2'" >> alacritty.toml
+echo "red = '0xff0000'" >> alacritty.toml
+echo "green = '0xd300c4'" >> alacritty.toml
+echo "yellow = '0xf57800'" >> alacritty.toml
+echo "blue = '0x123e7c'" >> alacritty.toml
+echo "magenta = '0x711c91'" >> alacritty.toml
+echo "cyan = '0x0abdc6'" >> alacritty.toml
+echo "white = '0xd7d7d5'" >> alacritty.toml
+cd 
+echo "reopen the your ALACRITTY terminal"
+    
+    
